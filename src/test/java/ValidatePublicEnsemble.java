b53506427b8e34100b11f2cc27fc70b56b91297a
floriankirmaier <florian.kirmaier@gmail.com>
2020-12-16 20:13:03 +0100

    Added exception for unit-test

--- src/test/java/ValidatePublicEnsemble.java
+++ src/test/java/ValidatePublicEnsemble.java
@@ -3,6 +3,7 @@ import com.javafxensemble.website.test.ValidateEnsemble;
 public class ValidatePublicEnsemble extends ValidateEnsemble {
 
     public ValidatePublicEnsemble() {
+        skippedClasses().add("com.dlsc.pdfviewfx.demo.PDFViewApp");
         skippedClasses().add("com.dlsc.gemsfx.demo.PaymentOptionTilesApp");
     }
 
